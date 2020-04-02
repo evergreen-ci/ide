e9c9b28ff9229eb6b44637dd975447823f6b1713
Ubuntu <ubuntu@ip-10-122-31-122.ec2.internal>
2020-04-02 18:53:41 +0000

    adapt to new code-server packaging

--- make_tarball.py
+++ make_tarball.py
@@ -10,6 +10,7 @@ import shutil
 import io
 import boto3
 from datetime import date
+from pathlib import Path
 
 # https://stackoverflow.com/questions/37071388/how-can-i-install-visual-studio-code-extensions-offline
 EXTENSION_DOWNLOAD_URL = "https://{publisher}.gallery.vsassets.io/_apis/public/gallery/publisher/{publisher}/extension/{extension_name}/{extension_version}/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage"
@@ -17,6 +18,7 @@ CODE_SERVER_RELEASES_URL = "https://api.github.com/repos/cdr/code-server/release
 CODE_SERVER_RELEASE_ASSET_URL = "https://api.github.com/repos/cdr/code-server/releases/assets/{asset_id}"
 
 def download_extensions(extensions, destination_dir):
+    Path(destination_dir).mkdir(parents=True, exist_ok=True)
     for extension in extensions:
         print("getting extension {}.{}".format(extension["publisher"], extension["name"]))
         url = EXTENSION_DOWNLOAD_URL.format(extension_name=extension["name"], publisher=extension["publisher"], extension_version=extension["version"])
@@ -54,14 +56,15 @@ def download_code_server(release, architecture, destination_dir):
     with tempfile.TemporaryDirectory() as temp_dir:
         with tarfile.open(fileobj=r.raw) as tf:
             tf.extractall(temp_dir)
-        shutil.copy(os.path.join(temp_dir, asset_name[:-7], "code-server"), destination_dir)
+        shutil.copytree(os.path.join(temp_dir, asset_name[:-7]), destination_dir)
 
 
-def make_tarball(input_dir, output_path):
+def make_tarball(code_server_dir, extensions_dir, output_path):
     print("creating tarball")
     with tarfile.open(output_path, mode="w:gz") as tar:
-        tar.add(input_dir, arcname="code-server")
-        tar.add("settings.json", arcname="code-server/settings.json")
+        tar.add(code_server_dir, arcname="code-server")
+        tar.add(code_server_dir, arcname="code-server/extension_packages")
+        tar.add("settings.json", arcname="code-server/User/settings.json")
 
 def get_extension_list(extension_json_path):
     with open(extension_json_path) as f:
@@ -84,10 +87,12 @@ def main():
 
     extensions = get_extension_list(args.extensions)
     with tempfile.TemporaryDirectory() as tempDir:
-        download_extensions(extensions, tempDir)
-        download_code_server(args.release, args.architecture, tempDir)
+        code_server_dir = os.path.join(tempDir, "code-server")
+        extensions_dir = os.path.join(tempDir, "extension_packages")
+        download_code_server(args.release, args.architecture, code_server_dir)
+        download_extensions(extensions, extensions_dir)
         output_name = "{date}_{architecture}_code-server.tgz".format(architecture=args.architecture, date=date.today().isoformat())
-        make_tarball(tempDir, os.path.join(args.destination, output_name))
+        make_tarball(code_server_dir, extensions_dir, os.path.join(args.destination, output_name))
 
     if args.s3_bucket is not None:
         upload_to_s3(args.s3_bucket, os.path.join(args.destination, output_name), "evergreen/vscode/{}".format(output_name))
