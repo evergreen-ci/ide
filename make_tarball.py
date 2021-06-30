64aa3d8a9dc51c58cd6aa54c90d2aeb3ffefeaac
Jonathan Brill <jonathan.brill@10gen.com>
2021-06-30 17:52:50 -0400

    EVG-13647 update code-server (#2)

--- make_tarball.py
+++ make_tarball.py
@@ -18,7 +18,9 @@ CODE_SERVER_RELEASES_URL = "https://api.github.com/repos/cdr/code-server/release
 CODE_SERVER_RELEASE_ASSET_URL = "https://api.github.com/repos/cdr/code-server/releases/assets/{asset_id}"
 ICON_PATH = "src/browser/media/"
 PRODUCT_PATH = "lib/vscode/product.json"
+PWA_MANIFEST_PATH = "src/browser/media/manifest.json"
 PRODUCT_NAME = "Evergreen - IDE"
+PRODUCT_NAME_SHORT = "IDE"
 
 def download_extensions(extensions, destination_dir):
     Path(destination_dir).mkdir(parents=True, exist_ok=True)
@@ -60,18 +62,28 @@ def download_code_server(release, architecture, destination_dir):
         with tarfile.open(fileobj=r.raw) as tf:
             tf.extractall(temp_dir)
         shutil.copytree(os.path.join(temp_dir, asset_name[:-7]), destination_dir)
+    
+    return release_info["name"]
 
 def customize_code_server(base_dir):
     print("replacing icons")
     for icon_file in os.listdir("icons"):
         shutil.copy(os.path.join("icons", icon_file), os.path.join(base_dir, ICON_PATH))
-    print("changing application name")
+    print("changing application name in product.json")
     with open(os.path.join(base_dir, PRODUCT_PATH)) as product_config:
         product_json = json.load(product_config)
-    product_json["nameShort"] = PRODUCT_NAME
+    product_json["nameShort"] = PRODUCT_NAME_SHORT
     product_json["nameLong"] = PRODUCT_NAME
     with open(os.path.join(base_dir, PRODUCT_PATH), 'w') as product_config:
         json.dump(product_json, product_config)
+
+    print("changing application name in PWA's manifest.json")
+    with open(os.path.join(base_dir, PWA_MANIFEST_PATH)) as pwa_manifest:
+        manifest_json = json.load(pwa_manifest)
+    manifest_json["short_name"] = PRODUCT_NAME_SHORT
+    manifest_json["name"] = PRODUCT_NAME
+    with open(os.path.join(base_dir, PWA_MANIFEST_PATH), 'w') as pwa_manifest:
+        json.dump(manifest_json, pwa_manifest)
 
 def make_tarball(code_server_dir, extensions_dir, output_path):
     print("creating tarball")
@@ -88,14 +100,14 @@ def get_extension_list(extension_json_path):
 def upload_to_s3(bucket, tarball_path, object_name):
     print("uploading to s3")
     s3_client = boto3.client("s3")
-    s3_client.upload_file(tarball_path, bucket, object_name)
+    s3_client.upload_file(tarball_path, bucket, object_name, ExtraArgs={"ACL": "public-read"})
 
 
 def main():
     parser = argparse.ArgumentParser(description="Create a code-server distribution for Evergreen virtual-workstations")
     parser.add_argument("extensions", help="JSON file listing extensions to include")
     parser.add_argument("--release", help="code-server release", default="latest")
-    parser.add_argument("--architecture", help="code-server binary for ARCHITECTURE", default="linux-x86_64", choices=["linux-x86_64", "linux-arm64", "darwin-x86_64", "alpine-x86_64", "alpine-arm64"])
+    parser.add_argument("--architecture", help="code-server binary for ARCHITECTURE", default="linux-amd64", choices=["linux-amd64", "linux-arm64", "macos-amd64"])
     parser.add_argument("--destination", help="Local directory to output the tarball", default=os.getcwd())
     parser.add_argument("--s3_bucket", help="s3 bucket to upload to")
     args = parser.parse_args()
@@ -104,10 +116,10 @@ def main():
     with tempfile.TemporaryDirectory() as tempDir:
         code_server_dir = os.path.join(tempDir, "code-server")
         extensions_dir = os.path.join(tempDir, "extension_packages")
-        download_code_server(args.release, args.architecture, code_server_dir)
+        upstream_version = download_code_server(args.release, args.architecture, code_server_dir)
         customize_code_server(code_server_dir)
         download_extensions(extensions, extensions_dir)
-        output_name = "{date}_{architecture}_code-server.tgz".format(architecture=args.architecture, date=date.today().isoformat())
+        output_name = "{date}_code-server-{upstream_version}-{architecture}.tgz".format(architecture=args.architecture, date=date.today().isoformat(), upstream_version=upstream_version)
         make_tarball(code_server_dir, extensions_dir, os.path.join(args.destination, output_name))
 
     if args.s3_bucket is not None:
